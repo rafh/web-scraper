ccc026fd346f90eee7c3c7b60f1f78af6a1ff951
rafael <rafae.heard@gmail.com>
2020-02-26 22:57:36 -0500

    remove require to add axios and handle errors

--- scrape.js
+++ scrape.js
@@ -1,7 +1,8 @@
-const request = require('request');
+const axios = require('axios')
 const cheerio = require('cheerio');
 const fs = require('fs');
 const inquirer = require('inquirer');
+const vomitEmoji = require('./vomit');
 const writeStream = fs.createWriteStream('index.html', { flags: 'a' });
 
 const questions = [{
@@ -12,7 +13,6 @@ const questions = [{
 
 let url = '';
 
-
 inquirer.prompt(questions).then(answers => {
 	url = answers['name'];
 
@@ -20,13 +20,11 @@ inquirer.prompt(questions).then(answers => {
 		console.log('...working')
 	})
 
-	const options = {
-		url: url
-	};
-
-	request(options, (error, response, html) => {
-		if (!error && response.statusCode === 200) {
-			const $ = cheerio.load(html);
+	axios
+	.get(url)
+	.then((response) => {
+		if (response.status === 200) {
+			const $ = cheerio.load(response.data);
 
 			const styleTag = $('style[type="text/css"]');
 			const linkStyle = $('link[rel=stylesheet]');
@@ -43,23 +41,57 @@ inquirer.prompt(questions).then(answers => {
 			})
 
 			writeStream.write(`
-				<head>
-					${headerLinks}
-					<style>${styles}</style>
-					<meta data-rh="true" name="viewport" content="width=device-width,minimum-scale=1,initial-scale=1">
-					<style>body{ background-color: white}</style>
-				</head>
-					
-				${article.html()}
-					
-				${/* build images */''}
-				<script>
-				document.querySelectorAll('noscript').forEach((x, i) => {
-					x.insertAdjacentHTML('afterend',  x.innerHTML)
-				})
-				</script>`
+			<head>
+				${headerLinks}
+				<style>${styles}</style>
+				<meta data-rh="true" name="viewport" content="width=device-width,minimum-scale=1,initial-scale=1">
+				<style>body{ background-color: white}</style>
+			</head>
+				
+			${article.html()}
+				
+			${/* build images */''}
+			<script>
+			document.querySelectorAll('noscript').forEach((x, i) => {
+				x.insertAdjacentHTML('afterend',  x.innerHTML)
+			})
+			</script>`
 			)
 		}
-
 	})
+	.catch((error) => {
+		console.log(error.code)
+		let errorMsg = 'Something went awry...';
+		if (error.code === 'ENOTFOUND') {
+			errorMsg = 'Not found...';
+		}
+		if(error.code === 'ECONNREFUSED') {
+			errorMsg = 'Connection refused...';
+		}
+		if (error) {
+			writeStream.write(`
+			<style>
+				div {
+					display: flex;
+					justify-content: center;
+					flex-direction: column;
+					align-items: center;
+					height: 100%;					
+				}
+				svg {
+					max-width: 500px;
+					width: 90%;
+				}
+				h1 {
+					font-family: Arial;
+					font-size: 4rem;
+				}
+			</style>
+			<div>
+				${vomitEmoji}
+				<h1>${errorMsg}</h1>
+			</div>`
+			)
+		}
+	});
 }) 
