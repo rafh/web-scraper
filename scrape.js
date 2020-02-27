34446bf8fd305376cdc8492b2e40ab9ada96f323
rafael <rafae.heard@gmail.com>
2020-02-26 21:58:01 -0500

    add user prompt

--- scrape.js
+++ scrape.js
@@ -1,27 +1,65 @@
 const request = require('request');
 const cheerio = require('cheerio');
 const fs = require('fs');
+const inquirer = require('inquirer');
 const writeStream = fs.createWriteStream('index.html', { flags: 'a' });
 
-const url = ''
+const questions = [{
+	type: 'input',
+	name: 'name',
+	message: "What's the url?",
+}]
 
-fs.truncate('./index.html', 0, function(){console.log('done')})
+let url = '';
 
-request(url, (error, response, html) => {
-	if (!error && response.statusCode === 200) {
-		const $ = cheerio.load(html);
 
-		const styleTag = $('style');
-		const article = $('article');
-		let styles = '';
+inquirer.prompt(questions).then(answers => {
+	url = answers['name'];
 
-		styleTag.each((__, elm) => {
-			styles += $(elm).html();
-		})
+	fs.truncate('./index.html', 0, () => {
+		console.log('...working')
+	})
 
-		writeStream.write(
-			`<style>${styles}</style>${article.html()}`
-		)
-	}
+	const options = {
+		url: url
+	};
 
-})
+	request(options, (error, response, html) => {
+		if (!error && response.statusCode === 200) {
+			const $ = cheerio.load(html);
+
+			const styleTag = $('style[type="text/css"]');
+			const linkStyle = $('link[rel=stylesheet]');
+			const article = $('article');
+			let styles = '';
+			let headerLinks = '';
+
+			styleTag.each((__, elm) => {
+				styles += $(elm).html();
+			})
+
+			linkStyle.each((__, elm) => {
+				headerLinks += `<link rel="stylesheet" type="text/css" href="${$(elm).attr('href')}">`;
+			})
+
+			writeStream.write(`
+				<head>
+					${headerLinks}
+					<style>${styles}</style>
+					<meta data-rh="true" name="viewport" content="width=device-width,minimum-scale=1,initial-scale=1">
+					<style>body{ background-color: white}</style>
+				</head>
+					
+				${article.html()}
+					
+				${/* build images */''}
+				<script>
+				document.querySelectorAll('noscript').forEach((x, i) => {
+					x.insertAdjacentHTML('afterend',  x.innerHTML)
+				})
+				</script>`
+			)
+		}
+
+	})
+}) 
